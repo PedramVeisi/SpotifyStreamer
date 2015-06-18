9a2db5e641d03e080d27854fbe722041a908d7e9
Pedram Veisi <pedramveisi@gmail.com>
2015-06-18 14:24:49 -0600

    Parcelable is implemented to store data locally.

--- app/src/main/java/ir/veisi/pedram/spotifystreamer/models/ArtistGist.java
+++ app/src/main/java/ir/veisi/pedram/spotifystreamer/models/ArtistGist.java
@@ -1,5 +1,9 @@
 package ir.veisi.pedram.spotifystreamer.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import kaaes.spotify.webapi.android.models.Image;
@@ -8,11 +12,22 @@ import kaaes.spotify.webapi.android.models.Image;
  * Created by pedram on 12/06/15.
  * This class is used to summarize the returned artist from Spotify and store the data we need.
  */
-public class ArtistGist {
+public class ArtistGist implements Parcelable {
+
     private String mArtistId;
     private String mArtistName;
     private List<Image> mArtistImages;
     private List<String> mArtistGenres;
+
+    // Read data from Parcel. Constructor auto-generated using Parcelable Generator Plugin
+    // https://github.com/mcharmas/android-parcelable-intellij-plugin
+    private ArtistGist(Parcel in) {
+        this.mArtistId = in.readString();
+        this.mArtistName = in.readString();
+        this.mArtistImages = new ArrayList<Image>();
+        in.readList(this.mArtistImages, List.class.getClassLoader());
+        this.mArtistGenres = in.createStringArrayList();
+    }
 
     public ArtistGist(String artistId, String artistName, List<Image> artistImages, List<String> artistGenres){
         this.mArtistId = artistId;
@@ -41,4 +56,29 @@ public class ArtistGist {
         return mArtistGenres;
     }
 
+    // Create a Parcel and save data in it.
+    // Following code auto-generated using Parcelable Generator Plugin
+    // https://github.com/mcharmas/android-parcelable-intellij-plugin
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mArtistId);
+        dest.writeString(this.mArtistName);
+        dest.writeList(this.mArtistImages);
+        dest.writeStringList(this.mArtistGenres);
+    }
+
+    public static final Parcelable.Creator<ArtistGist> CREATOR = new Parcelable.Creator<ArtistGist>() {
+        public ArtistGist createFromParcel(Parcel source) {
+            return new ArtistGist(source);
+        }
+
+        public ArtistGist[] newArray(int size) {
+            return new ArtistGist[size];
+        }
+    };
 }
