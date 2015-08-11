34de453fc00b7292d2f157cfc98e324e921b39e4
Pedram Veisi <pedramveisi@gmail.com>
2015-08-12 02:29:43 +0430

    Unregister broadcast receiver in main activity

--- app/src/main/java/si/vei/pedram/spotifystreamer/activities/MainActivity.java
+++ app/src/main/java/si/vei/pedram/spotifystreamer/activities/MainActivity.java
@@ -57,6 +57,12 @@ public class MainActivity extends AppCompatActivity implements ArtistSearchFragm
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
